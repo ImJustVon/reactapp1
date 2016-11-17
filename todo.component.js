4c0250042b2c7d0da5d2a6bc5c7b1e2ee927fb3b
Vaughn Prosser <vaughnprosser@gmail.com>
2016-11-16 22:52:28 -0600

    todo component

---
+++ todo.component.js
@@ -0,0 +1,20 @@
+const TodoForm = ({ addTodo }) => {
+  // Input tracker
+  let input;
+
+  return (
+
+<div>
+      <input ref={node => {
+        input = node;
+      }} />
+
+      <button onClick={() => {
+        addTodo(input.value);
+        input.value = '';
+      }}>
+        +
+      </button>
+    </div>
+  );
+};
